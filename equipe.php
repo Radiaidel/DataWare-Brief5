e16c4b7f1d15c351a02e8c562d2827f80d68f716
radiaide <test@gmail.com>
2023-11-17 23:40:43 +0100

    Update

--- equipe.php
+++ equipe.php
@@ -29,77 +29,133 @@ if (!$res) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Liste des équipes</title>
+    <style>
+        html,
+        body,
+        .intro {
+
+            height: 100%;
+        }
+
+        body {
+            background-color: #282a41;
+        }
+
+        table td,
+        table th {
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            overflow: hidden;
+        }
+
+        .mask-custom {
+            background-color: #3d3f54;
+            border-radius: 2em;
+            border: 2px solid rgba(255, 255, 255, 0.05);
+            background-clip: padding-box;
+            box-shadow: 10px 10px 10px rgba(46, 54, 68, 0.03);
+        }
+
+        h1 {
+            color: #e1b204;
+            font-size: 3em;
+            font-weight: 600;
+            margin: 1em;
+            text-align: center;
+        }
+
+        .btnmodifier {
+            border-radius: 20px;
+            background: #e1b204;
+        }
+
+        .btnajouter {
+            border-radius: 20px;
+            border: 1px solid #e1b204;
+            color: #e1b204;
+            background-color: #3d3f54;
+        }
+
+        input {
+            border: none;
+            border-radius: 10px;
+            color: white;
+            background: transparent;
+        }
+    </style>
 </head>
 
 <body>
 
     <div class="container mt-5">
-        <h2>Liste des équipes</h2>
+        <h1>Liste des équipes</h1>
 
-        <button type="button" class="btn btn-success mb-3" id="ajouterMembreBtn">Ajouter un membre</button>
+        <button type="button" class="btn btnajouter mb-3 float-end" id="ajouterMembreBtn">Ajouter une équipe</button>
 
-        <table class="table" id="tableContainer">
-            <thead class="table-dark">
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Nom</th>
-                    <th scope="col">Date de création</th>
-                    <th scope="col"></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-             while ($ligne = mysqli_fetch_assoc($res)) {
-                echo "<tr>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12">
+                    <div class="card mask-custom">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-borderless text-white mb-0">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">Nom</th>
+                                            <th scope="col">Date de création</th>
+                                            <th scope="col">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        while ($ligne = mysqli_fetch_assoc($res)) {
+                                            echo "<tr>
                         <form action='equipe.php' method='post'>
                            
-                            <td>                            <input type='text' name='id_equipe' value='" . $ligne['id_equipe'] . "'>
-</td>
-                            <td>
-                                <input type='text' name='nom_equipe' value='" . $ligne['nom_equipe'] . "'>
-                            </td>
-                            <td>" . $ligne['date_creation'] . "</td>
-                            <td>
-                                <button type='submit' class='btn btn-primary' name='modifierEquipe'>Modifier</button>
-                                <button type='submit' class='btn btn-danger' name='supprimerEquipe'>Supprimer</button>  
-                            </td>
-                        </form>
-                      </tr>";
+                                    <td><input type='text' name='id_equipe' value='" . $ligne['id_equipe'] . "'>
+                                    </td>
+                                    <td>
+                                        <input type='text' name='nom_equipe' value='" . $ligne['nom_equipe'] . "'>
+                                    </td>
+                                    <td>" . $ligne['date_creation'] . "</td>
+                                    <td>
+                                        <button type='submit' class='btn btnmodifier' name='modifierEquipe'>Modifier</button>
+                                    </td>
+                                </form>
+                            </tr>";
+                                        }
+
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <?php
+        // Connectez-vous à la base de données ici
+        
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $idEquipe = $_POST["id_equipe"];
+            $nouveauNomEquipe = $_POST["nom_equipe"];
+
+            if (isset($_POST["modifierEquipe"])) {
+                // Effectuez la mise à jour du nom de l'équipe
+                $sqlUpdate = "UPDATE equipe SET nom_equipe = '$nouveauNomEquipe' WHERE id_equipe = $idEquipe";
+                $resUpdate = mysqli_query($conn, $sqlUpdate);
+
+                if ($resUpdate) {
+                    echo "Équipe mise à jour avec succès!";
+                } else {
+                    echo "Erreur lors de la mise à jour de l'équipe : " . mysqli_error($conn);
+                }
             }
-            
-                ?>
-            </tbody>
-        </table>
-<?php
-// Connectez-vous à la base de données ici
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $idEquipe = $_POST["id_equipe"];
-    $nouveauNomEquipe = $_POST["nom_equipe"];
-
-    if (isset($_POST["modifierEquipe"])) {
-        // Effectuez la mise à jour du nom de l'équipe
-        $sqlUpdate = "UPDATE equipe SET nom_equipe = '$nouveauNomEquipe' WHERE id_equipe = $idEquipe";
-        $resUpdate = mysqli_query($conn, $sqlUpdate);
-
-        if ($resUpdate) {
-            echo "Équipe mise à jour avec succès!";
-        } else {
-            echo "Erreur lors de la mise à jour de l'équipe : " . mysqli_error($conn);
         }
-    } elseif (isset($_POST["supprimerEquipe"])) {
-        // Effectuez la suppression de l'équipe
-        $sqlDelete = "DELETE FROM equipe WHERE id_equipe = $idEquipe";
-        $resDelete = mysqli_query($conn, $sqlDelete);
-
-        if ($resDelete) {
-            echo "Équipe supprimée avec succès!";
-        } else {
-            echo "Erreur lors de la suppression de l'équipe : " . mysqli_error($conn);
-        }
-    }
-}
-?>
+        ?>
 
 
         <div class="modal" id="ajoutMembreModal" tabindex="-1" role="dialog">
@@ -146,7 +202,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </div>
         </div>
 
-        <!-- ... (votre code HTML) -->
+
+
+
+
+
+        <div class="modal" id="ajoutMembreModal" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Ajouter une équipe</h5>
+                    </div>
+                    <div class="modal-body">
+                        <form action="equipe.php" method="post">
+                            <div class="mb-3">
+                                <label for="nom_equipe" class="form-label">Nom de l'équipe</label>
+                                <input type="text" class="form-control" id="nom_equipe" name="nom_equipe" required>
+                            </div>
+                            <button type="submit" class="btn btn-success" name="ajouterEquipe">Ajouter</button>
+                        </form>
+
+
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </div>
 
--- membre.php
+++ membre.php
@@ -6,11 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <style>
+        <style>
         html,
         body,
         .intro {
+
             height: 100%;
+        }
+
+        body {
+            background-color: #282a41;
         }
 
         table td,
@@ -21,67 +26,95 @@
         }
 
         .mask-custom {
-            background: rgba(24, 24, 16, 0.7);
+            background-color: #3d3f54;
             border-radius: 2em;
             border: 2px solid rgba(255, 255, 255, 0.05);
             background-clip: padding-box;
             box-shadow: 10px 10px 10px rgba(46, 54, 68, 0.03);
+        }
+
+        h1 {
+            color: #e1b204;
+            font-size: 3em;
+            font-weight: 600;
+            margin: 1em;
+            text-align: center;
+        }
+
+        .btnmodifier {
+            border-radius: 20px;
+            background: #e1b204;
+        }
+
+        .btnajouter {
+            border-radius: 20px;
+            border: 1px solid #e1b204;
+            color: #e1b204;
+            background-color: #3d3f54;
+        }
+
+        input {
+            border: none;
+            border-radius: 10px;
+            color: white;
+            background: transparent;
         }
     </style>
     <title>Document</title>
 </head>
 
 <body>
-
-
-
     <section class="intro">
-        <div class="bg-image h-100">
-            <div class="mask d-flex align-items-center h-100">
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-12">
-                            <div class="card mask-custom">
-                                <div class="card-body">
-                                    <div class="table-responsive">
-                                        <table class="table table-borderless text-white mb-0">
-                                            <thead>
-                                                <tr>
-                                                    <th scope="col">#</th>
-                                                    <th scope="col">nom</th>
-                                                    <th scope="col">prenom</th>
-                                                    <th scope="col">email</th>
-                                                    <th scope="col">telephone</th>
-                                                    <th scope="col">role</th>
-                                                    <th scope="col">status</th>
-                                                    <th scope="col">nom equipe</th>
-                                                    <th scope="col">action</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <?php
-                                                ini_set('display_errors', 'off'); //pour ne pas afficher les erreurs et warnings
-                                                $server_name = "localhost";
-                                                $user = "root";
-                                                $password = "";
-                                                $db = "dataware";
+    <h1>Liste des membres</h1>
 
-                                                $conn = mysqli_connect($server_name, $user, $password, $db);
-
-                                                if (!$conn) {
-                                                    die("La connexion à la base de données a échoué : " . mysqli_connect_error());
-                                                }
+        <button type="button" class="btn btnajouter mb-3 float-end" id="ajouterMembreBtn">Ajouter un membre</button>
 
 
-                                                $sql = "SELECT id_membre,nom_membre,prenom_membre,email,telephone,role, status_membre, nom_equipe FROM membre inner join equipe on membre.id_equipe=equipe.id_equipe;";
-                                                $res = mysqli_query($conn, $sql);
-                                                if (!$res) {
-                                                    die("Erreur dans la requête : " . mysqli_error($conn));
 
-                                                }
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12">
+                    <div class="card mask-custom">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-borderless text-white mb-0">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">nom</th>
+                                            <th scope="col">prenom</th>
+                                            <th scope="col">email</th>
+                                            <th scope="col">telephone</th>
+                                            <th scope="col">role</th>
+                                            <th scope="col">status</th>
+                                            <th scope="col">nom equipe</th>
+                                            <th scope="col">action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        ini_set('display_errors', 'off'); //pour ne pas afficher les erreurs et warnings
+                                        $server_name = "localhost";
+                                        $user = "root";
+                                        $password = "";
+                                        $db = "dataware";
 
-                                                while ($ligne = mysqli_fetch_assoc($res)) {
-                                                    echo "<tr>
+                                        $conn = mysqli_connect($server_name, $user, $password, $db);
+
+                                        if (!$conn) {
+                                            die("La connexion à la base de données a échoué : " . mysqli_connect_error());
+                                        }
+
+
+                                        $sql = "SELECT id_membre,nom_membre,prenom_membre,email,telephone,role, status_membre, nom_equipe FROM membre inner join equipe on membre.id_equipe=equipe.id_equipe;";
+                                        $res = mysqli_query($conn, $sql);
+                                        if (!$res) {
+                                            die("Erreur dans la requête : " . mysqli_error($conn));
+
+                                        }
+
+                                        while ($ligne = mysqli_fetch_assoc($res)) {
+                                            echo "<tr>
                                                             <th scope=\"row\">" . $ligne['id_membre'] . "</td>
                                                             <td>" . $ligne['nom_membre'] . "</td>
                                                             <td>" . $ligne['prenom_membre'] . "</td>
@@ -91,25 +124,128 @@
                                                             <td>" . $ligne['status_membre'] . "</td>
                                                             <td>" . $ligne['nom_equipe'] . "</td>
                                                             <td>
-                                                                <button type='submit' class='btn btn-primary' name='modifierEquipe'>Modifier</button>
-                                                                <button type='submit' class='btn btn-danger' name='supprimerEquipe'>Supprimer</button>  
+                                                                <button type='submit' class='btn btnmodifier' name='modifierEquipe' id='modifierEquipe'>Modifier</button>
+                                                                <button type='submit' class='btn btnmodifier' name='supprimerEquipe'>Supprimer</button>  
                                                             </td>
                                                         </tr>";
-                                                }
-
-                                                mysqli_close($conn);
-                                                ?>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
+                                        }
+                                        ?>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
+
+        <div class="modal" id="ajoutMembreModal" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Ajouter un membre</h5>
+                    </div>
+                    <div class="modal-body">
+                        <form action="membre.php" method="post">
+                            <div class="mb-3">
+                                <input type="text" class="form-control" id="nom_membre" name="nom_membre"
+                                    placeholder="Nom" required>
+                                <input type="text" class="form-control" id="prenom_membre" name="prenom_membre"
+                                    placeholder="Prenom" required>
+                                <input type="email" class="form-control" id="email_membre" name="email_membre"
+                                    placeholder="Email" required>
+                                <input type="text" class="form-control" id="telephone_membre" name="telephone_membre"
+                                    placeholder="Telephone" required>
+                                <select class="form-select" id="role_membre" name="role_membre"
+                                    aria-label="Default select example">
+                                    <option selected>Role</option>
+                                    <option value="Chef de projet">Chef de projet</option>
+                                    <option value="Concepteur UX/UI">Concepteur UX/UI </option>
+                                    <option value="Testeur">Testeur</option>
+                                    <option value="Analyste commercial">Analyste commercial</option>
+                                    <option value="Administrateur de base de données">Administrateur de base de données
+                                    </option>
+                                    <option value="Responsable des opérations">Responsable des opérations</option>
+                                    <option value="Scrum Master">Scrum Master</option>
+                                    <option value="Support technique">Support technique </option>
+                                    <option value="Spécialiste en sécurité">Spécialiste en sécurité </option>
+                                </select>
+
+                                <select class="form-select" id="statut_membre" name="statut_membre"
+                                    aria-label="Default select example">
+                                    <option selected>statut</option>
+                                    <option value="Actif">Actif </option>
+                                    <option value="En congé">En congé</option>
+                                    <option value="En attente">En attente</option>
+                                    <option value="En probation">En probation</option>
+                                    <option value="En formation">En formation</option>
+                                    <option value="Indisponible">Indisponible </option>
+                                    <option value="En retraite">En retraite</option>
+                                </select>
+
+                                <select class="form-select" id="equipe_membre" name="equipe_membre"
+                                    aria-label="Default select example">
+                                    <option selected>Equipe</option>
+                                    <?php
+
+                                    $sql = "SELECT id_equipe,nom_equipe FROM equipe;";
+                                    $res = mysqli_query($conn, $sql);
+                                    while ($ligne = mysqli_fetch_assoc($res)) {
+                                        echo "<option value='" . $ligne['id_equipe'] . "'>" . $ligne['nom_equipe'] . "</option>";
+                                    }
+
+                                    ?>
+                                </select>
+
+
+
+
+                            </div>
+                            <button type="submit" class="btn btn-success" name="ajoutermembre">Ajouter</button>
+                        </form>
+
+                        <?php
+                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                            if (isset($_POST["ajoutermembre"])) {
+                                $nom_membre = $_POST["nom_membre"];
+                                $prenom_membre = $_POST["prenom_membre"];
+                                $email_membre = $_POST["email_membre"];
+                                $telephone_membre = $_POST["telephone_membre"];
+                                $role_membre = $_POST["role_membre"];
+                                $statut_membre = $_POST["statut_membre"];
+                                $equipe_membre = $_POST["equipe_membre"];
+
+                                // Insérer le membre dans la base de données
+                                $sqlInsertMembre = "INSERT INTO `membre`(`id_membre`, `nom_membre`, `prenom_membre`, `email`, `telephone`, `role`, `status_membre`, `id_equipe`) VALUES (4356,'$nom_membre', '$prenom_membre', '$email_membre', '$telephone_membre', '$role_membre', '$statut_membre', $equipe_membre)";
+                                $resInsertMembre = mysqli_query($conn, $sqlInsertMembre);
+
+                                if ($resInsertMembre) {
+                                    echo "Membre ajouté avec succès!";
+                                } else {
+                                    echo "Erreur lors de l'ajout du membre : " . mysqli_error($conn);
+                                }
+                            }
+                        }
+                        ?>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script>
+            document.getElementById('ajouterMembreBtn').addEventListener('click', function () {
+                var modal = document.getElementById('ajoutMembreModal');
+                modal.style.display = 'block';
+            });
+
+        </script>
     </section>
+
 </body>
 
 </html>
